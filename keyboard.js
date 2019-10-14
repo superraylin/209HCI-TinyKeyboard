4822cfcb5576a209587b0e1acb73e602fbd07f8d
Super Ray <ray199246@yahoo.com>
2019-10-13 22:47:34 -0700

    generate keyboard.js

---
+++ keyboard.js
@@ -0,0 +1,7 @@
+function showabc() {
+  document.getElementById("button_abc").firstChild.data = "A";
+}
+
+function restoreabc(){
+    document.getElementById("button_abc").firstChild.data = "abc";
+}
