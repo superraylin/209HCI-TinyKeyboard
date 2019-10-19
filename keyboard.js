4911d30d23cf5dcc2b3ba1f4753a9f0b090c77f6
Super Ray <ray199246@yahoo.com>
2019-10-19 15:42:41 -0700

    change to pointer test

--- keyboard.js
+++ keyboard.js
@@ -1,6 +1,60 @@
+function InitializeApp(){
+ ["pointerdown", "pointerup"].forEach(function(e) {
+            document.getElementById("button_abc").addEventListener(e, PointerHandlerABC);
+//             document.getElementById("button_rst").addEventListener(e, PointerHandler);
+//             document.getElementById("button_xyz").addEventListener(e, PointerHandler);
+//             document.getElementById("button_ghi").addEventListener(e, PointerHandler);
+        });
+  
+}
+
+function PointerHandlerABC(event){
+  console.log('abc')
+}
+// function PointerHandler(event) {
+//     event.preventDefault();
+
+//     if (event.type == "pointerdown" && event.button === 0)
+//         mousePressed = true;
+
+//     if (mousePressed || event.type == "pointerup" || (event.buttons & 1)) {
+//         var fakeTouch = {
+//             identifier: event.pointerId,
+//             pageX : event.pageX,
+//             pageY : event.pageY,
+//             radiusX : event.width / 2,
+//             radiusY : event.height / 2 ,
+//             force : event.pressure,
+//         };
+//         var eventType = event.type == "pointerdown" ? "touchstart" :
+//             event.type == "pointerup" ? "touchend" : "touchmove";
+//         event.preventDefault();
+
+//         console.log(event.pageX,event.pageY);
+//         //drawTouch(fakeTouch, eventType, false);
+
+// //         if (drawCoalesced && event.getCoalescedEvents) {
+// //             var points = event.getCoalescedEvents();
+// //             for(let coalesced of points) {
+// //                 fakeTouch.pageX = coalesced.pageX;
+// //                 fakeTouch.pageY = coalesced.pageY + 50;
+// //                 fakeTouch.radiusX = coalesced.width / 2;
+// //                 fakeTouch.radiusY = coalesced.height / 2;
+// //                 fakeTouch.force = coalesced.pressure;
+
+// //                 drawTouch(fakeTouch, eventType, true);
+// //             }
+// //         }
+//     }
+
+//     if (event.type == "pointerup")
+//         mousePressed = false;
+// }
+
+
 function showabc(event) {
   hideall();
-  console.log(event)
+  //console.log(event)
   document.getElementById("button_rst").firstChild.data = "a";
   document.getElementById("button_xyz").firstChild.data = "b";
   document.getElementById("button_ghi").firstChild.data = "c";
@@ -33,6 +87,9 @@ function restoreall(){
 })
 }
 
+function restoreabc(){
+  
+}
 function restorerst(){
     let current_text = document.getElementById("button_rst").firstChild.data 
     //console.log(current_text)
@@ -66,10 +123,6 @@ function restoreghi(){
     };
 }
 
-function dosomething(){
-  document.getElementById("button_abc").firstChild.data = "A";
-  console.log("dosomething");
-}
 
 function xy_report(e){
   //console.log(e)
