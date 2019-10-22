785fa5c5cc0ea52536ceb2cec0f3a0a3aadc7db5
ray199246 <ray199246@yahoo.com>
2019-10-21 22:29:38 -0700

    text entry done and comments added

--- keyboard.js
+++ keyboard.js
@@ -4,7 +4,7 @@ var tapped = false;
 let two_touch = false;
 let touch_counts = 0;
 
-
+/******load event listener to each keyboar_key*******/
 function Init(){
   document.addEventListener('contextmenu', event => event.preventDefault());
  ["pointerdown", "pointerup"].forEach(function(e) {
@@ -12,64 +12,58 @@ function Init(){
             document.getElementById("button_def").addEventListener(e, PointerHandlerDEF);
             document.getElementById("button_ghi").addEventListener(e, PointerHandlerGHI);
             document.getElementById("button_jkl").addEventListener(e, PointerHandlerJKL);
-            document.getElementById("button_mn").addEventListener( e,  PointerHandlerMN);
-            document.getElementById("button_opq").addEventListener(e, PointerHandlerOPQ);
-            document.getElementById("button_rst").addEventListener(e, PointerHandlerRST);
-            document.getElementById("button_uvw").addEventListener(e, PointerHandlerUVW);
-            document.getElementById("button_xyz").addEventListener(e, PointerHandlerXYZ);
+            document.getElementById("button_mno").addEventListener( e,  PointerHandlerMN);
+            document.getElementById("button_pqr").addEventListener(e, PointerHandlerOPQ);
+            document.getElementById("button_stu").addEventListener(e, PointerHandlerRST);
+            document.getElementById("button_vwx").addEventListener(e, PointerHandlerUVW);
+            document.getElementById("button_yz").addEventListener(e, PointerHandlerXYZ);
             
         });
 }
 
-
+/**************Pointer Handler begin******************/
 
 function PointerHandlerABC(event){
-
   event.preventDefault();
 
   if (event.type === "pointerdown"){
       hideall();
-      document.getElementById("button_rst").firstChild.data = "a";
-      document.getElementById("button_xyz").firstChild.data = "b";
-      document.getElementById("button_ghi").firstChild.data = "c";
+      showButtonText("a","b","c");
       delete_swipe_down(event);
   }
   if(event.type ==="pointerup") touch_counts -=1;
   if (event.type === "pointerup" && event.isPrimary){
-    enter_text(event,"c","","a","b");
+    enter_text(event,"c","b","a","");
   }
 }
+
 function PointerHandlerDEF(event){
     event.preventDefault();
   if (event.type === "pointerdown"){
       hideall();
-      document.getElementById("button_rst").firstChild.data = "d";
-      document.getElementById("button_xyz").firstChild.data = "e";
-      document.getElementById("button_ghi").firstChild.data = "f";
+      showButtonText("d","e","f");
+
       delete_swipe_down(event);
   }
-    if(event.type ==="pointerup") touch_counts -=1;
+  if(event.type ==="pointerup") touch_counts -=1;
   if (event.type === "pointerup"&& event.isPrimary){
-    //delete_swipe_up(event);
-    enter_text(event,"f","","d","e");
-
+    enter_text(event,"f","e","d","");
   }
 }
+
 function PointerHandlerGHI(event){
   event.preventDefault();
   if (event.type === "pointerdown"){
       hideall();
-      document.getElementById("button_abc").firstChild.data = "g";
-      document.getElementById("button_rst").firstChild.data = "h";
-      document.getElementById("button_xyz").firstChild.data = "i";
+      showButtonText("g","h","i");
       delete_swipe_down(event);
-      console.log(touch_counts);
+
   }
   if(event.type ==="pointerup") touch_counts -=1;
   if (event.type === "pointerup"&& event.isPrimary){
 
     if(!delete_swipe_up(event)){
-      enter_text(event,"","g","h","i");
+      enter_text(event,"i","h","g","");
 
     }
   }
@@ -79,14 +73,13 @@ function PointerHandlerJKL(event){
   event.preventDefault();
   if (event.type === "pointerdown"){
       hideall();
-      document.getElementById("button_rst").firstChild.data = "j";
-      document.getElementById("button_xyz").firstChild.data = "k";
-      document.getElementById("button_ghi").firstChild.data = "l";
+      showButtonText("j","k","l");
+
       delete_swipe_down(event);
   }
   if(event.type ==="pointerup") touch_counts -=1;
   if (event.type === "pointerup"&& event.isPrimary){
-    enter_text(event,"l","","j","k");
+    enter_text(event,"l","k","j","");
 
   }
 }
@@ -94,60 +87,46 @@ function PointerHandlerJKL(event){
 function PointerHandlerMN(event){
   event.preventDefault();
   if (event.type === "pointerdown"){
-//       if(!tapped){
-//         tapped = setTimeout(function(){
+
           hideall();
-          document.getElementById("button_rst").firstChild.data = "m";
-          document.getElementById("button_ghi").firstChild.data = "n";
-          document.getElementById("button_xyz").firstChild.data = "sp";
+          showButtonText("m","n","o");
+
           delete_swipe_down(event);
-//           tapped = null;
-//         }, 300);
-
-//       }else{
-//         clearTimeout(tapped); //stop single tap callback
-//         tapped=null;
-//         document.getElementById("txt_area").value += " ";
-//       }
-
       
   }
   if(event.type ==="pointerup") touch_counts -=1;
   if (event.type ==="pointerup"&& event.isPrimary){
-    enter_text(event,"n","","m"," ");
+    enter_text(event,"o","n","m","");
   }
 }
 
 function PointerHandlerOPQ(event){
-    event.preventDefault();
+  event.preventDefault();
   if (event.type === "pointerdown"){
       hideall();
-      document.getElementById("button_abc").firstChild.data = "o";
-      document.getElementById("button_rst").firstChild.data = "p";
-      document.getElementById("button_xyz").firstChild.data = "q";
+      showButtonText("p","q","r");
       delete_swipe_down(event);
   }
     if(event.type ==="pointerup") touch_counts -=1;
   if (event.type === "pointerup"&& event.isPrimary){
      if(!delete_swipe_up(event)){
-       enter_text(event,"","o","p","q");
+       enter_text(event,"r","q","p","");
       }
   }
 }
 
 function PointerHandlerRST(event){
   event.preventDefault();
-  //console.log(event.type);
+
   if (event.type === "pointerdown"){
       hideall();
-      document.getElementById("button_abc").firstChild.data = "r";
-      document.getElementById("button_ghi").firstChild.data = "s";
-      document.getElementById("button_xyz").firstChild.data = "t";
+      showButtonText("s","t","u");
+
       delete_swipe_down(event);
   }
   if(event.type ==="pointerup") touch_counts -=1;
   if (event.type === "pointerup"&& event.isPrimary){
-    enter_text(event,"s","r","","t");
+    enter_text(event,"u","t","s","");
   }
 }
 
@@ -155,34 +134,49 @@ function PointerHandlerUVW(event){
     event.preventDefault();
   if (event.type === "pointerdown"){
       hideall();
-      document.getElementById("button_abc").firstChild.data = "u";
-      document.getElementById("button_ghi").firstChild.data = "v";
-      document.getElementById("button_xyz").firstChild.data = "w";
+      showButtonText("v","w","x");
       delete_swipe_down(event);
   }
   if(event.type ==="pointerup") touch_counts -=1;
   if (event.type ==="pointerup"&& event.isPrimary){
-    enter_text(event,"v","","u","w");
+    enter_text(event,"x","w","v","");
 
   }
 }
 function PointerHandlerXYZ(event){
     event.preventDefault();
   if (event.type === "pointerdown"){
+      
+      /*Delay for double tap*/
+    if(!tapped){
+      tapped = setTimeout(function(){
       hideall();
-      document.getElementById("button_rst").firstChild.data = "x";
-      document.getElementById("button_abc").firstChild.data = "y";
-      document.getElementById("button_ghi").firstChild.data = "z";
-      delete_swipe_down(event);
+      showButtonText("del","y","z");
+
+          delete_swipe_down(event);
+          tapped = null;
+        }, 200);
+
+      }else{
+        clearTimeout(tapped); //stop timeout callback
+        tapped=null;
+        document.getElementById("txt_area").value += " ";
+      }
+
   }
   if(event.type ==="pointerup") touch_counts -=1;
   if (event.type === "pointerup"&& event.isPrimary){
      if(!delete_swipe_up(event)){
-       enter_text(event,"z","y","x","");
+       enter_text(event,"z","y","del","");
       }
   }
 }
 
+
+/**************Pointer Handler end***********************/
+
+
+/***************double tap function begin ****************/
 function delete_swipe_down(event){
   touch_counts += 1;
   if (touch_counts >=2){
@@ -195,8 +189,6 @@ function delete_swipe_down(event){
 }
 
 function delete_swipe_up(event){
-  console.log(touch_counts)
-  //touch_counts -=1;
   if(two_touch === true && event.isPrimary === true){
     let cursor_pos = position_button(event.clientX,event.clientY);
     if(cursor_pos === 2 || cursor_pos ===3){
@@ -208,18 +200,26 @@ function delete_swipe_up(event){
     return true;
   }
 }
+/***************double tap function end ****************/
 
+
+/***********enter/delete text respect to pointerup position****/
 function enter_text(event,ch1,ch2,ch3,ch4){
     let button_name = event.target.id
     let current_text = document.getElementById(button_name).firstChild.data; 
     let cursor_pos = position_button(event.clientX,event.clientY);
-    //console.log(cursor_pos);
+//     console.log(cursor_pos);
     switch(cursor_pos){
       case 1: document.getElementById("txt_area").value +=  ch1;
               break;
       case 2: document.getElementById("txt_area").value +=  ch2;
               break;
-      case 3: document.getElementById("txt_area").value += ch3;
+      case 3: if(ch3 == "del"){
+                let str = document.getElementById("txt_area").value;
+                document.getElementById("txt_area").value = str.substring(0, str.length - 1);
+              }else{
+                document.getElementById("txt_area").value += ch3;
+              }
               break;
       case 4: document.getElementById("txt_area").value += ch4;
               break;
@@ -228,7 +228,15 @@ function enter_text(event,ch1,ch2,ch3,ch4){
 }
 
 
-//clrear all key text
+/********************assign letter to corner keys*****/
+function showButtonText(stu_ch,abc_ch,ghi_ch){
+      document.getElementById("button_abc").firstChild.data = abc_ch;
+      document.getElementById("button_ghi").firstChild.data = ghi_ch;
+      document.getElementById("button_stu").firstChild.data = stu_ch;
+
+}
+
+/***********clrear all key text**************/
 function hideall(){
     let all_buttons = document.getElementsByClassName("keyboard_key");
     Array.prototype.forEach.call(all_buttons, function(single_button) {
@@ -241,7 +249,6 @@ function hideall(){
 function restoreall(){
     let all_buttons = document.getElementsByClassName("keyboard_key");
     Array.prototype.forEach.call(all_buttons, function(single_button) {
-//     let current_text = single_button.firstChild.data
     let str = single_button.id
     let next_text = str.substring(7,str.length)
 
@@ -249,21 +256,27 @@ function restoreall(){
 })
 }
 
-// return quardant on release cursor position
+// return (1~4) quadrant on release cursor position
 function position_button(x,y){
-  if (x >= 12 && x <= 62){
-    if(y >= 123 && y<= 170){
-      return 2; //second quatrum
-    }else if(y > 170 && y< 217){
-      return 3; //third quartum
+  var rect = document.getElementById("keyboardbox").getBoundingClientRect();
+  console.log(rect.left, rect.right,rect.top, rect.bottom, );
+
+  let x_mid = (rect.left+ rect.right)/2;
+  let y_mid = (rect.top+ rect.bottom)/2;
+
+  if (x >= rect.left && x <= x_mid){
+    if(y >= 123 && y<= y_mid){
+      return 2; //second quadrant
+    }else if(y > y_mid && y< rect.bottom){
+      return 3; //third quadrant
     } else return 0; //out of bound
   }
-   else if(x > 62 && x < 123){
+   else if(x > x_mid && x < rect.right){
 
-       if(y >= 123 && y<= 170){
-        return 1; //second quatrum
-      }else if(y > 170 && y< 217){
-        return 4; //third quartum
+      if(y >= rect.top && y<= y_mid){
+        return 1; //first quadrant
+      }else if(y > y_mid && y< rect.bottom){
+        return 4; //fourth quadrant
       } else return 0; //out of bound
   }
    else { return 0; //out of bound}
